dccd0b52c306b53222a3d29c9576f801fc3a8c55
gopika-velayudhan <gopikakv627@gmail.com>
2023-11-09 16:51:33 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function sendMail() {
     var params={
         name:document.getElementById("name").value,
-        email:document.getElementById("Email").value,
+        email:document.getElementById("email").value,
         message:document.getElementById("message").value,
     };
 
@@ -9,11 +9,11 @@ function sendMail() {
 const templateID ="template_nvaua4k";
 
 emailjs.send(serviceID,templateID,params)
-c
+
 .then(
     res =>{
         document.getElementById("name").value="";
-        document.getElementById("Email").value="";
+        document.getElementById("email").value="";
         document.getElementById("message").value="";
         console.log(res);
         alert("your message send successfully");
